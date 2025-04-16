b68d5a57b5dfcc13344ca51778fb4f245aa40ce7
Black <163615105+BLA4CKK@users.noreply.github.com>
2025-04-16 19:06:52 -0300

    Add files via upload

---
+++ Projeto Ana-20250416T220435Z-001/Projeto Ana/login.js
@@ -0,0 +1,52 @@
+function verificarSenha() {
+    let senhaCorreta = "027752";
+    let easter_egg = {
+        "meupeixinhofavorito123": {
+            mensagem: "Pescou meu coração 🐟",
+            imagem: "img2.jpg"
+        },
+        "bocó": {
+            mensagem: "Bocó é você oxi 😡😡",
+            imagem: "img7.jpg"
+
+        },
+        "Lookism":{
+            mensagem: "Pikaaa",
+            imagem: "lookism.jpg"
+        },
+        "Ghotam":{
+            mensagem: "Seu cavaleiro das trevas chegou 🦇💘",
+            imagem: "Batman.jpg"
+        },
+        "Ninjago":{
+            mensagem: "Quer ser minha Skylor gatinha??",
+            imagem: "Ninjago.jpg"
+        }
+    }
+    let senhaDigitada = document.getElementById("senha").value;
+    let mensagem = document.getElementById("mensagem");
+    const imagem = document.getElementById("imagem-peixe");
+
+    if (senhaDigitada === senhaCorreta) {
+        mensagem.style.color = "green";
+        mensagem.innerText = "Senha correta! Redirecionando...";
+
+        setTimeout(() => {
+            window.location.href = "Home.html";
+        }, 1500);
+    } 
+
+    else if (senhaDigitada in easter_egg){
+        const ovo = easter_egg[senhaDigitada]
+        mensagem.style.color = "green"
+        mensagem.innerText = ovo.mensagem
+        imagem.src = ovo.imagem
+    }
+    
+    
+    else {
+        mensagem.style.color = "red";
+        mensagem.innerText = "Senha incorreta! Tente novamente.";
+    }
+}
+z
